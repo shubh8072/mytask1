950659d9ae6b248289557e5e74fda53171cbf66c
shubh8072 <64468715+shubh8072@users.noreply.github.com>
2020-07-17 23:40:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA2YIADDJH7JJZFIEA"
-  secret_key = "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"
+  profile = "shubhgupta94"
 }
 resource "aws_instance" "os3" {
   ami = "ami-0447a12f28fddb066"
